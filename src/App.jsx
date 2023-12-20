6e7a888527dde648c639aa83ec10e3815a65d92c
shahlahassan123 <ahlan.shahla@gmail.com>
2023-12-19 19:41:17 -0500

    Fix images

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,22 @@ import { useState } from 'react'
 
 const App = () => {
   const [items, setItems] = useState([
-    { id: 1, img: '/img/html.png', stat: "" },
-    { id: 1, img: '/img/html.png', stat: "" },
-    { id: 2, img: '/img/css.png', stat: "" },
-    { id: 2, img: '/img/css.png', stat: "" },
-    { id: 3, img: '/img/js.png', stat: "" },
-    { id: 3, img: '/img/js.png', stat: "" },
-    { id: 4, img: '/img/scss.png', stat: "" },
-    { id: 4, img: '/img/scss.png', stat: "" },
-    { id: 5, img: '/img/react.png', stat: "" },
-    { id: 5, img: '/img/react.png', stat: "" },
-    { id: 6, img: '/img/vue.png', stat: "" },
-    { id: 6, img: '/img/vue.png', stat: "" },
-    { id: 7, img: '/img/angular.png', stat: "" },
-    { id: 7, img: '/img/angular.png', stat: "" },
-    { id: 8, img: '/img/nodejs.png', stat: "" },
-    { id: 8, img: '/img/nodejs.png', stat: "" }
+    { id: 1, img: './img/html.png', stat: "" },
+    { id: 1, img: './img/html.png', stat: "" },
+    { id: 2, img: './img/css.png', stat: "" },
+    { id: 2, img: './img/css.png', stat: "" },
+    { id: 3, img: './img/js.png', stat: "" },
+    { id: 3, img: './img/js.png', stat: "" },
+    { id: 4, img: './img/scss.png', stat: "" },
+    { id: 4, img: './img/scss.png', stat: "" },
+    { id: 5, img: './img/react.png', stat: "" },
+    { id: 5, img: './img/react.png', stat: "" },
+    { id: 6, img: './img/vue.png', stat: "" },
+    { id: 6, img: './img/vue.png', stat: "" },
+    { id: 7, img: './img/angular.png', stat: "" },
+    { id: 7, img: './img/angular.png', stat: "" },
+    { id: 8, img: './img/nodejs.png', stat: "" },
+    { id: 8, img: './img/nodejs.png', stat: "" }
 ].sort(()=>Math.random()-0.5))   //shuffling the array
 
 const [prev,setPrev] = useState(-1)
