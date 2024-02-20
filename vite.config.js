a72f3748c752cbcb1a2ec04c1388c0d2640a0315
shahlahassan123 <ahlan.shahla@gmail.com>
2024-02-20 16:08:31 -0600

    Vercel.json added

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Memory_Game_React/', // Adjust the base path accordingly
-  build: {
-    outDir: 'dist', // Specify the output directory for the build
-  },
-});
+})
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/Memory_Game_React/', // Adjust the base path accordingly
+//   build: {
+//     outDir: 'dist', // Specify the output directory for the build
+//   },
+// });
